c0e0df9566beb6135057346b8f95bd27aef3dfb5
gosthnam <gosthnam@gmail.com>
2023-08-06 23:55:45 +0200

    character background image

--- pages/characters/[id]/index.vue
+++ pages/characters/[id]/index.vue
@@ -1,23 +1,29 @@
 <template>
-  <div v-if="character" class="grid grid-rows-1 grid-cols-2 gap-4 w-full h-auto mx-40 my-20 text-white">
-    <div>
-      <div id="name">
-        {{ character.name }} {{ character.rarity }}*
+  <div class="flex w-full h-full justify-center">
+    <div class="flex w-1/6"></div>
+    <div class="flex w-4/6 rounded-3xl bg-gradient-to-br from-slate-600 to-slate-700 my-10 p-10">
+      <div v-if="character" class="grid grid-rows-1 grid-cols-2 gap-4 w-full h-full text-white">
+        <div class="bg-contain bg-no-repeat bg-center" :style="{ backgroundImage: 'url(../../assets/images/character/' + character.name + '.webp)'}">
+          <div id="name">
+            {{ character.name }} {{ character.rarity }}*
+          </div>
+          <div id="level">
+            Level: {{ character.level }}
+          </div>
+          <div id="ascension">
+            Ascension: {{ character.promotion }}
+          </div>
+        </div>
+        <div class="grid grid-rows-3 grid-cols-2 gap-4">
+          <Relic v-for="relic in character.relics" :relic="relic">
+          </Relic>
+        </div>
       </div>
-      <div id="level">
-        Level: {{ character.level }}
-      </div>
-      <div id="ascension">
-        Ascension: {{ character.promotion }}
+      <div v-else>
+        not found
       </div>
     </div>
-    <div class="grid grid-rows-3 grid-cols-2 gap-4">
-      <Relic v-for="relic in character.relics" :relic="relic">
-      </Relic>
-    </div>
-  </div>
-  <div v-else>
-    not found
+    <div class="flex w-1/6"></div>
   </div>
 </template>
 
